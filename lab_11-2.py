ccb7a3d928a5d90e2b557f7dac53a2d6545fd375
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-02-10 10:37:51 -0500

    Submit Lab 11

--- lab_11-2.py
+++ lab_11-2.py
@@ -1,9 +1,6 @@
 # Author: CRS 02/08/21
 file = open("alma_mater.txt", "r")
 while True:
-     line = file.readline()
-     if len(line) == 0:
-           break
-     print (line, end="")
-     print(" ")
-file.close()
+     line = file.readlines()
+     for lines in reversed(line):
+           print(lines)
