d4ec012933f97ca3bcc565d2ff20a7044aaf0c15
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-03-07 09:39:23 -0500

    Submit Cycle 11 Labs

--- lab_11-1.py
+++ lab_11-1.py
@@ -1,9 +1,12 @@
 # Author: CRS 02/08/21
+# Create file variable
 file = open("alma_mater.txt", "r")
+# Create while statement to edit file
 while True:
      line = file.readline()
      if len(line) == 0:
            break
      print (line, end="")
      print(" ")
+     # Close the file
 file.close()
--- lab_11-2.py
+++ lab_11-2.py
@@ -1,5 +1,7 @@
 # Author: CRS 02/08/21
+# Create file variable
 file = open("alma_mater.txt", "r")
+# Create while statement to edit file
 while True:
      line = file.readlines()
      for lines in reversed(line):
